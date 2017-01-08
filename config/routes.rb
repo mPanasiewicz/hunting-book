90814527343c9c66346746e35ba8dff10dd06442
mPanasiewicz <grunek1@gmail.com>
2017-01-08 18:19:16 +0100

    Add bootstrap4 layout with favicons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/build_in' => 'high_voltage/pages#show', id: 'build_in'
+  get '/homepage/', to: 'homepage#index', as: 'homepage'
 end
